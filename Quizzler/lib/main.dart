0c87ece1e9513eb84a1d4a6162b1e723e90efeaa
Eric Mullen <ericmullen1989@gmail.com>
2020-01-23 18:23:44 -0600

    Add files via upload

---
+++ Quizzler/lib/main.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  int totalScore = 0;
+  QuizBrain quizBrain = QuizBrain();
+  List<Icon> score = [];
+
+  void checkAnswer(bool userAnswer) {
+    setState(() {
+      if (userAnswer == quizBrain.getAnswer()) {
+        score.add(Icon(Icons.check, color: Colors.green));
+        totalScore++;
+      } else {
+        score.add(Icon(Icons.close, color: Colors.red));
+      }
+
+      if (quizBrain.isFinished() == true) {
+        int total = quizBrain.getQuestionsLength();
+        Alert(
+                context: context,
+                title: "QUIZ COMPLETE!",
+                desc: "Your score is: $totalScore/$total!")
+            .show();
+
+        totalScore = 0;
+        score.clear();
+        quizBrain.resetQuiz();
+      } else {
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(false);
+              },
+            ),
+          ),
+        ),
+        Row(children: score),
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
---
+++ Quizzler/lib/question.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Question {
+  String questionText;
+  bool answer;
+
+  Question(this.questionText, this.answer);
+}
